948bec5899906cf1ef852750c89593f08a23d785
Mahesh Gvelly <maheshgvelly@outlook.com>
2015-11-05 19:54:18 -0500

    Initial Commit

---
+++ TappingGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TappingGame
+//
+//  Created by Mahesh on 11/5/15.
+//  Copyright © 2015 Mahesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TappingGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TappingGame
+//
+//  Created by Mahesh on 11/5/15.
+//  Copyright © 2015 Mahesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
